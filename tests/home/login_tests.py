b88f2ee298c0c78cab8cc842244dd5ca01101c69
ChrisStreadbeck <han0987@gmail.com>
2019-09-12 11:38:28 -0600

    automation framework initial commit

---
+++ tests/home/login_tests.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import unittest
+
+from login_page import LoginPage
+
+class LoginTests(unittest.TestCase):
+
+    def test_validLogin(self):
+        baseUrl = "https://learn.letskodeit.com/p/practice"
+        driverLocation = "chromedriver.exe"
+        driver = webdriver.Chrome(driverLocation)
+        driver.maximize_window()
+        driver.implicitly_wait(3)
+        driver.get(baseUrl)
+        
+        lp = LoginPage(driver)
+        lp.login("test@email.com", "abcabc")
+
+        userIcon = driver.find_element(By.XPATH, "//*[@id=\"navbar\"]//img[@class=\"gravatar\"]")
+
+        if userIcon is not None:
+            print("Login Successful")
+        else:
+            print("Login Failed")
